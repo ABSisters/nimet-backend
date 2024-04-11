0f72db65491e3f77beaa42ec2c02f29aaf3a5c28
Raquel Tsuji <raqueltsuji@gmail.com>
2024-04-11 12:00:12 -0300

    first commit

---
+++ src/main/java/ABSisters/nimet/NimetApplication.java
@@ -0,0 +1,13 @@
+package ABSisters.nimet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ABSisters/nimet/NimetApplicationTests.java
@@ -0,0 +1,13 @@
+package ABSisters.nimet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
