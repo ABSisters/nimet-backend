b2c2d63decb51b87247e22f63c88f66b45f0c9e2
Isa Sousa <isabella.santana@aluno.ifsp.edu.br>
2024-05-02 22:02:30 -0300

    Atualização do endpoint login

---
+++ target/classes/db/migration/V1__criar_usuarios.sql
@@ -0,0 +1,13 @@
+CREATE TABLE usuarios (
+    usuario_id varchar2(32) NOT NULL,
+    nome varchar2(100) NOT NULL,
+    username varchar2(20) NOT NULL,
+    email varchar2(256) NOT NULL,
+    email_valido number(1) NOT NULL,
+    data_nascimento date NOT NULL,
+    senha varchar2(64) NOT NULL,
+    curso varchar2(20) NOT NULL CHECK( curso IN ('ELETRONICA','INFORMATICA','MECANICA')),
+    tipo_usuario varchar2(20) NOT NULL CHECK( tipo_usuario IN ('ESTUDANTE','MODERADOR')),
+    data_criado date NOT NULL,
+    CONSTRAINT usuario_pk PRIMARY KEY (usuario_id)
+);
