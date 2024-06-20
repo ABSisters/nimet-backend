c815e01af140f17902e958ac871133e94df76b74
Raquel Tsuji <raqueltsuji@gmail.com>
2024-06-20 14:32:23 -0300

    adicionando get das perguntas dde um determinado curso (pegando o curso do usuario)

--- src/main/java/absisters/nimet/repository/PerguntaRepository.java
+++ src/main/java/absisters/nimet/repository/PerguntaRepository.java
@@ -1,10 +1,14 @@
 package absisters.nimet.repository;
 
+import absisters.nimet.domain.Curso;
 import absisters.nimet.domain.Pergunta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PerguntaRepository extends JpaRepository<Pergunta, String> {
     Pergunta findByPerguntaId(String id);
+    List<Pergunta> findByCurso(Curso curso);
 }
