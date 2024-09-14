a79ad01175a7527926224170fd345a8047de771d
Raquel Tsuji <raqueltsuji@gmail.com>
2024-09-14 20:19:11 -0300

    retirando cmetodonÃao utilizado no application tests

--- src/test/java/ABSisters/nimet/NimetApplicationTests.java
+++ src/test/java/ABSisters/nimet/NimetApplicationTests.java
@@ -1,13 +1,7 @@
 package ABSisters.nimet;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NimetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
--- target/generated-sources/annotations/absisters/nimet/dto/Mapper/QuestaoMapperImpl.java
+++ target/generated-sources/annotations/absisters/nimet/dto/Mapper/QuestaoMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-14T16:24:29-0300",
+    date = "2024-09-14T19:54:11-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
